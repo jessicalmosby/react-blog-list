11c732e1e53c203dd7dbdff356c560fe4a598f78
Jessica Mosby <jessicamosby@mac.com>
2023-01-17 18:10:31 -0500

    was attempting strech goal-spent too much time, need to start on from scratch

--- src/services/getBlogs.js
+++ src/services/getBlogs.js
@@ -4,3 +4,7 @@ export async function getBlogs() {
   const resp = await client.from('blogs').select('*');
   return checkError(resp);
 }
+
+// export async function getAuthors(author_id) {
+//   const resp = await client.from('authors').select('name').match({ id: author_id });
+// }
