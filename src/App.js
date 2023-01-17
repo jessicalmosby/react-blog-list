cfa63334d86234e35be2ff71c73875f057d762c6
Jessica Mosby <jessicamosby@mac.com>
2023-01-17 15:21:21 -0500

    created getBlogs and useBlogs. ready for take off

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
-import Main from './components/Main/Main.js';
+import Main from './components/layout/Main/Main.js';
 
 function App() {
   return (
